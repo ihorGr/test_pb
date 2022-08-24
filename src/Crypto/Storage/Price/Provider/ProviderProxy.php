560687d9579f4cb61a178caddd38715feab1e526
ihor <grad6901@gmail.com>
2022-08-24 11:51:08 +0300

    refactoring of denormalizing

--- src/Crypto/Storage/Price/Provider/ProviderProxy.php
+++ src/Crypto/Storage/Price/Provider/ProviderProxy.php
@@ -6,6 +6,7 @@ namespace App\Crypto\Storage\Price\Provider;
 
 use App\Crypto\Request\RequestInterface;
 use App\Crypto\Storage\Price\Provider\DTO\PriceDto;
+use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
 use Symfony\Component\Serializer\Normalizer\PropertyNormalizer;
 use Symfony\Component\Serializer\Serializer;
 
@@ -19,19 +20,21 @@ class ProviderProxy extends Provider implements ProviderInterface
     {
         $pricesList = parent::getPrices($request);
 
-        $pricesCollection = [];
-
-        foreach ($pricesList as $itemPrice) {
-            try {
-                $normalizers = [new PropertyNormalizer()];
-                $serializer = new Serializer($normalizers);
-
-                $pricesCollection[] = $serializer->denormalize($itemPrice, PriceDto::class);
-            } catch (\Throwable $e) {
-                throw new \RuntimeException($e->getMessage());
-            }
-        }
+        $pricesCollection = $this->denormalizeList($pricesList);
 
         return $pricesCollection;
+    }
+
+    /**
+     * @param array[] $pricesList
+     * @return PriceDto[]
+     * @throws \Symfony\Component\Serializer\Exception\ExceptionInterface
+     */
+    private function denormalizeList(array $pricesList): array
+    {
+        $normalizers = [new PropertyNormalizer(), new ArrayDenormalizer()];
+        $serializer = new Serializer($normalizers);
+
+        return $serializer->denormalize($pricesList, PriceDto::class.'[]');
     }
 }
